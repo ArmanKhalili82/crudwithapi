61a147f5fe9a82f6b5d094e1bf3b3f1883f91d5d
ArmanKhalili82 <hykhalili@yahoo.com>
2023-10-30 23:35:35 +0100

    map

--- src/features/users/UserList.js
+++ src/features/users/UserList.js
@@ -29,8 +29,9 @@ const UserList = () => {
  
 
   return (
-    const renderCard = () => users.map(user => (
-      <div className="bg-gray-300 p-5 flex items-center justify-between" key={user.id}>
+    {users.map(user => (
+      <div>
+        <div className="bg-gray-300 p-5 flex items-center justify-between" key={user.id}>
         <div>
           <h3 className="font-bold text-lg text-gray-700">{user.name}</h3>
           <span className="font-normal text-gray-600">{user.email}</span>
@@ -52,13 +53,14 @@ const UserList = () => {
           </button>
         </div>
       </div>
-    ))
     <div>
       <Link to="/Addedit/new"><Button>Add User</Button></Link>
       <div className="grid gap-5 md:grid-cols-2">
-        {users.length ? renderCard() : <p className="text-center col-span-2 text-gray-700 font-semibold">No User Available</p>}
+        {/* {users.length ? renderCard() : <p className="text-center col-span-2 text-gray-700 font-semibold">No User Available</p>} */}
       </div>
     </div>
+      </div>
+    ))}
   )
 }
 
